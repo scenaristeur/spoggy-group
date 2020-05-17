57127d718fa3bc60c154a7693ba9201fae0373e1
David <scenaristeur@gmail.com>
2020-05-17 19:32:34 +0200

    pages hola

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.12c0e9768c0948cdb0e85d15df057de9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.fb228c5fb70c0fdd626fbdc681f6b6ab.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).

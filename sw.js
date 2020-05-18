0c9d116309055e083b21aedd2d45350ca0124f4f
David <scenaristeur@gmail.com>
2020-05-18 02:37:59 +0200

    add gouv meeting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.fb228c5fb70c0fdd626fbdc681f6b6ab.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.b2bbab0f84eed90c67ee6bd0de41c72d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).

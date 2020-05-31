01ad695a78de088de629a45f33543b418b19b0b6
David <scenaristeur@gmail.com>
2020-05-31 15:39:53 +0200

    tension details

--- precache-manifest.6e607908e7b98ffd078becb3024aac60.js
+++ precache-manifest.6e607908e7b98ffd078becb3024aac60.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "6a7f52f96208fab5d03e",
+    "revision": "bd77518775c8ec0341e5",
     "url": "main.css"
   },
   {
-    "revision": "6a7f52f96208fab5d03e",
+    "revision": "bd77518775c8ec0341e5",
     "url": "views/main.js"
   },
   {
@@ -20,20 +20,12 @@ self.__precacheManifest = [
     "url": "vendor/custom-elements-es5-adapter.js"
   },
   {
-    "revision": "53fbd9e794ec54db453fa8215f8744a7",
-    "url": "libs/rdf-easy.bundle.js"
-  },
-  {
     "revision": "cc1382088863aa74eda6e6c8820df514",
     "url": "css/bootstrap/bootstrap.min.css"
   },
   {
-    "revision": "1a1d335b42f6f91037342d576887fa98",
-    "url": "libs/eve.custom.js"
-  },
-  {
-    "revision": "35a9c0cff2bb82890b850f239c1f21b2",
-    "url": "libs/solid-auth-client.bundle.js"
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "vendor/webcomponents-loader.js"
   },
   {
     "revision": "fb2608da8f301832531117b05f4acdb3",
@@ -48,10 +40,6 @@ self.__precacheManifest = [
     "url": "vendor/bundles/webcomponents-sd-ce.js"
   },
   {
-    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
-    "url": "libs/solid-file-client.bundle.js"
-  },
-  {
     "revision": "e2f0b54cb1564b7467ce56bab4945e77",
     "url": "vendor/bundles/webcomponents-sd.js"
   },
@@ -60,40 +48,36 @@ self.__precacheManifest = [
     "url": "vendor/webcomponents-bundle.js"
   },
   {
-    "revision": "e044a63e034bf10304dad73138b8c74b",
-    "url": "vendor/webcomponents-loader.js"
-  },
-  {
     "revision": "b1b1555a1e6a20d43d88f858d222b832",
     "url": "dist-popup/popup-template.html"
-  },
-  {
-    "revision": "dbc24b87bc6ab266990002bbbfb30f92",
-    "url": "libs/vis-network.min.js"
-  },
-  {
-    "revision": "07383e347d6beadac2693f770fd1f26b",
-    "url": "libs/solid-query-ldflex.bundle.js"
   },
   {
     "revision": "7a4b08594a85103b6735977c6a10cfbf",
     "url": "dist-popup/popup.html"
   },
   {
+    "revision": "53fbd9e794ec54db453fa8215f8744a7",
+    "url": "libs/rdf-easy.bundle.js"
+  },
+  {
+    "revision": "1a1d335b42f6f91037342d576887fa98",
+    "url": "libs/eve.custom.js"
+  },
+  {
+    "revision": "35a9c0cff2bb82890b850f239c1f21b2",
+    "url": "libs/solid-auth-client.bundle.js"
+  },
+  {
+    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
+    "url": "libs/solid-file-client.bundle.js"
+  },
+  {
+    "revision": "07383e347d6beadac2693f770fd1f26b",
+    "url": "libs/solid-query-ldflex.bundle.js"
+  },
+  {
     "revision": "a473d17109009d21b4648f16e666a63e",
     "url": "img/compose.png"
-  },
-  {
-    "revision": "af4868f2eeec483dea3d6a5f242d1095",
-    "url": "img/flow.png"
-  },
-  {
-    "revision": "9adea9fc0bd06d09f31185f0b927d007",
-    "url": "img/icon-128.png"
-  },
-  {
-    "revision": "0cff3aebfb1463f0e052511d90a96772",
-    "url": "img/icon-16.png"
   },
   {
     "revision": "90af4a5d863208fba09a5f84a4ce94de",
@@ -108,6 +92,26 @@ self.__precacheManifest = [
     "url": "img/checkControl.png"
   },
   {
+    "revision": "af4868f2eeec483dea3d6a5f242d1095",
+    "url": "img/flow.png"
+  },
+  {
+    "revision": "9adea9fc0bd06d09f31185f0b927d007",
+    "url": "img/icon-128.png"
+  },
+  {
+    "revision": "0cff3aebfb1463f0e052511d90a96772",
+    "url": "img/icon-16.png"
+  },
+  {
+    "revision": "f65c8035f4233b911b5c196bbdaa65e6",
+    "url": "img/icon-180.png"
+  },
+  {
+    "revision": "d68fcb5b46f2295e3f304a950609d207",
+    "url": "img/icon-256.png"
+  },
+  {
     "revision": "68aded03ecec133a11f41116c9dbe236",
     "url": "img/icon-32.png"
   },
@@ -116,16 +120,16 @@ self.__precacheManifest = [
     "url": "img/icon-48.png"
   },
   {
-    "revision": "d68fcb5b46f2295e3f304a950609d207",
-    "url": "img/icon-256.png"
-  },
-  {
-    "revision": "f65c8035f4233b911b5c196bbdaa65e6",
-    "url": "img/icon-180.png"
-  },
-  {
     "revision": "9b5e91ababe48f96596f6b3494d2fa3e",
     "url": "img/icons/favicon.png"
+  },
+  {
+    "revision": "104b3fda1e992ee77cf88e2ba8542a4a",
+    "url": "img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "91a9501d74924f5da51f71dceffe6db5",
+    "url": "img/icons/icon-192x192.png"
   },
   {
     "revision": "61c0e8d2ba57ccd56501339f3eb6713d",
@@ -136,12 +140,8 @@ self.__precacheManifest = [
     "url": "img/icons/icon-512x512.png"
   },
   {
-    "revision": "91a9501d74924f5da51f71dceffe6db5",
-    "url": "img/icons/icon-192x192.png"
-  },
-  {
-    "revision": "104b3fda1e992ee77cf88e2ba8542a4a",
-    "url": "img/icons/icon-144x144.png"
+    "revision": "10dde2ee22997667361195d883274e3a",
+    "url": "img/favicon.ico"
   },
   {
     "revision": "62b3a769457f5cf5e535e058fdcb1efd",
@@ -152,32 +152,44 @@ self.__precacheManifest = [
     "url": "img/talk.png"
   },
   {
-    "revision": "08ad7a4a944e7564adf9414e9626f501",
-    "url": "css/fontawesome/css/all.min.css"
-  },
-  {
     "revision": "d076a239e97f39c1cfbf42cc5f9e3d51",
     "url": "img/trustedApps.png"
+  },
+  {
+    "revision": "dbc24b87bc6ab266990002bbbfb30f92",
+    "url": "libs/vis-network.min.js"
+  },
+  {
+    "revision": "fb4c753bff382564db12c367a745f51c",
+    "url": "img/faviconLoux.ico"
   },
   {
     "revision": "cd7111706f6aef7104e641f631f7f7d2",
     "url": "css/fontawesome/css/brands.css"
   },
   {
-    "revision": "da9aca32ed693ed28f5dcf9dbcb47ba5",
-    "url": "css/fontawesome/css/all.css"
+    "revision": "08ad7a4a944e7564adf9414e9626f501",
+    "url": "css/fontawesome/css/all.min.css"
   },
   {
-    "revision": "10dde2ee22997667361195d883274e3a",
-    "url": "img/favicon.ico"
+    "revision": "da9aca32ed693ed28f5dcf9dbcb47ba5",
+    "url": "css/fontawesome/css/all.css"
   },
   {
     "revision": "c0e6c5279c8424e068a91de2604d0f4c",
     "url": "css/fontawesome/css/brands.min.css"
   },
   {
+    "revision": "8515ea89b91072607e476ee6a6c1218c",
+    "url": "css/fontawesome/css/fontawesome.css"
+  },
+  {
     "revision": "2eabdc9bff8bff556f95f7dfe6711f23",
     "url": "css/fontawesome/css/regular.css"
+  },
+  {
+    "revision": "3e9e0e28b383f828743cb81eccfc069f",
+    "url": "css/fontawesome/css/solid.css"
   },
   {
     "revision": "f65791dea3d1e31cd6a5f53a3708e098",
@@ -192,32 +204,28 @@ self.__precacheManifest = [
     "url": "css/fontawesome/css/solid.min.css"
   },
   {
-    "revision": "8515ea89b91072607e476ee6a6c1218c",
-    "url": "css/fontawesome/css/fontawesome.css"
-  },
-  {
     "revision": "56bd90e8063dfb67194acbbd299c7833",
     "url": "css/fontawesome/css/svg-with-js.css"
-  },
-  {
-    "revision": "a4979986b22144315e388f23fa652918",
-    "url": "css/fontawesome/css/v4-shims.css"
-  },
-  {
-    "revision": "3e9e0e28b383f828743cb81eccfc069f",
-    "url": "css/fontawesome/css/solid.css"
-  },
-  {
-    "revision": "a881b9c284c7d93e43400f90057cae6e",
-    "url": "css/fontawesome/css/v4-shims.min.css"
   },
   {
     "revision": "79fa4870b77b8fbcdcf5089eb2ceede6",
     "url": "css/fontawesome/css/svg-with-js.min.css"
   },
   {
+    "revision": "a4979986b22144315e388f23fa652918",
+    "url": "css/fontawesome/css/v4-shims.css"
+  },
+  {
+    "revision": "a881b9c284c7d93e43400f90057cae6e",
+    "url": "css/fontawesome/css/v4-shims.min.css"
+  },
+  {
     "revision": "5063b105c7646c8043d58c5289f02cca",
     "url": "css/fontawesome/webfonts/fa-brands-400.eot"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "css/fontawesome/webfonts/fa-brands-400.woff2"
   },
   {
     "revision": "06147b6cd88c7346cecd1edd060cd5de",
@@ -232,80 +240,76 @@ self.__precacheManifest = [
     "url": "css/fontawesome/webfonts/fa-regular-400.eot"
   },
   {
-    "revision": "cccc9d29470e879e40eb70249d9a2705",
-    "url": "css/fontawesome/webfonts/fa-brands-400.woff2"
-  },
-  {
     "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
     "url": "css/fontawesome/webfonts/fa-regular-400.woff"
-  },
-  {
-    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
-    "url": "css/fontawesome/webfonts/fa-solid-900.eot"
-  },
-  {
-    "revision": "9443c1e7fdf95d739af3f0901224f90a",
-    "url": "css/fontawesome/webfonts/fa-regular-400.svg"
-  },
-  {
-    "revision": "f5f2566b93e89391da4db79462b8078b",
-    "url": "css/fontawesome/webfonts/fa-regular-400.woff2"
-  },
-  {
-    "revision": "333bae208dc363746961b234ff6c2500",
-    "url": "css/fontawesome/webfonts/fa-solid-900.woff"
-  },
-  {
-    "revision": "44d537ab79f921fde5a28b2c1636f397",
-    "url": "css/fontawesome/webfonts/fa-solid-900.woff2"
-  },
-  {
-    "revision": "2a0e9aae3999b944dd6e3b14f61051ce",
-    "url": "css/img/network/acceptDeleteIcon.png"
-  },
-  {
-    "revision": "0bff33a5fd7ec390235476b4859747a0",
-    "url": "css/fontawesome/webfonts/fa-solid-900.ttf"
   },
   {
     "revision": "65b286af947c0d982ca01b40e1fcab38",
     "url": "css/fontawesome/webfonts/fa-regular-400.ttf"
   },
   {
-    "revision": "dd0baa69a69937600dc3bf035f3873c4",
-    "url": "css/img/network/backIcon.png"
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "css/fontawesome/webfonts/fa-regular-400.woff2"
   },
   {
-    "revision": "bbfbf2e8eaa8ebbce8524769ac0a5bc0",
-    "url": "css/img/network/downArrow.png"
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "css/fontawesome/webfonts/fa-regular-400.svg"
   },
   {
-    "revision": "d5267b8db2498e44bd567bd12e07576b",
-    "url": "css/img/network/connectIcon.png"
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "css/fontawesome/webfonts/fa-solid-900.eot"
   },
   {
-    "revision": "02d321edc6e03aea1675001899d8f479",
-    "url": "css/img/network/deleteIcon.png"
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "css/fontawesome/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "css/fontawesome/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "2a0e9aae3999b944dd6e3b14f61051ce",
+    "url": "css/img/network/acceptDeleteIcon.png"
   },
   {
     "revision": "a1a2d01bcd034270a3bc92176edfb66c",
     "url": "css/img/network/addNodeIcon.png"
   },
   {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "css/img/network/backIcon.png"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "css/fontawesome/webfonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "css/img/network/cross.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "css/img/network/connectIcon.png"
+  },
+  {
     "revision": "1999beb15a7bc0ef8c84ee7d33af4985",
     "url": "css/img/network/cross2.png"
   },
   {
-    "revision": "d5422321c4f6feed4081891051f9a6b2",
-    "url": "css/img/network/editIcon.png"
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "css/img/network/deleteIcon.png"
   },
   {
-    "revision": "5a379c46656d7a393c8f1ab293e46def",
-    "url": "css/img/network/minus.png"
+    "revision": "bbfbf2e8eaa8ebbce8524769ac0a5bc0",
+    "url": "css/img/network/downArrow.png"
   },
   {
     "revision": "8965662a4348c548189b97493c21a19e",
     "url": "css/img/network/leftArrow.png"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "css/img/network/editIcon.png"
   },
   {
     "revision": "4f88ef172e7bc7e98e24fc7df5a05c4b",
@@ -316,28 +320,24 @@ self.__precacheManifest = [
     "url": "css/img/network/plus.png"
   },
   {
-    "revision": "5f78e86f895f5a5cc1472cc57db154c5",
-    "url": "css/img/network/zoomExtends.png"
+    "revision": "5a379c46656d7a393c8f1ab293e46def",
+    "url": "css/img/network/minus.png"
   },
   {
     "revision": "f0aa0566b9f39334bd886d27d6a4cfe1",
     "url": "css/img/network/upArrow.png"
   },
   {
+    "revision": "5f78e86f895f5a5cc1472cc57db154c5",
+    "url": "css/img/network/zoomExtends.png"
+  },
+  {
     "revision": "9dc193bfa1182f65e9a5ac520b549bdf",
     "url": "css/offcanvas.css"
   },
   {
-    "revision": "fb4c753bff382564db12c367a745f51c",
-    "url": "img/faviconLoux.ico"
-  },
-  {
     "revision": "cd581bf745fcb764067a261dc111978a",
     "url": "css/vis-network.min.css"
-  },
-  {
-    "revision": "260c9c6535065674bb5e59a73f894abb",
-    "url": "css/img/network/cross.png"
   },
   {
     "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.16304932d51b900626e2d07ff98c925e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.6e607908e7b98ffd078becb3024aac60.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
